54417f58a76b546f52889b6840362b97d96404a2
Danil Samsonov <thrashdev@protonmail.com>
2025-07-17 09:35:00 +0600

    styling

--- src/components/react/AppointmentForm.tsx
+++ src/components/react/AppointmentForm.tsx
@@ -52,8 +52,11 @@ export const AppointmentForm = ({ service }: { service: ServiceValidated }) => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="mt-5">
-        <div className="flex flex-col space-y-5 md:flex-row md:space-x-5">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-5 w-full items-center justify-center space-y-5"
+      >
+        <div className="flex flex-col space-y-5 md:flex-row md:space-x-5 justify-center">
           <FormField
             control={form.control}
             name="calendarDate"
