8fddf4ac624037e9e363b73cc5e884314708a723
Danil Samsonov <thrashdev@protonmail.com>
2025-08-14 13:05:27 +0600

    added login mockup

--- src/components/react/BusinessLoginForm.tsx
+++ src/components/react/BusinessLoginForm.tsx
@@ -14,28 +14,31 @@ import { useForm } from "react-hook-form";
 import { Card } from "@/ui/card";
 import { Button } from "@/ui/button";
 
-const FormSchema = z.object({
-  name: z.string().nonempty({ message: i18n.t("form.brand_name_empty") }),
-  description: z.string(),
-  email: z
-    .email({ message: i18n.t("form.email_invalid") })
-    .nonempty({ message: i18n.t("form.email_empty") }),
-  phone_number: z.string().nonempty({ message: i18n.t("form.phone_empty") }),
-});
+const FormSchema = z
+  .object({
+    email: z
+      .email({ message: i18n.t("form.email_invalid") })
+      .nonempty({ message: i18n.t("form.email_empty") }),
+    password: z.string().min(4),
+    confirmPassword: z.string().min(4),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: i18n.t("form.passwords_dont_match"),
+    path: ["confirmPassword"],
+  });
 
-type BusinessSignUpFormValues = z.infer<typeof FormSchema>;
-const onSubmit = async (data: BusinessSignUpFormValues) => {
+type BusinessLoginFormValues = z.infer<typeof FormSchema>;
+const onSubmit = async (data: BusinessLoginFormValues) => {
   console.log(data);
 };
 
-export const BusinessSignUpForm = () => {
-  const form = useForm<BusinessSignUpFormValues>({
+export const BusinessLoginForm = () => {
+  const form = useForm<BusinessLoginFormValues>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      description: "",
       email: "",
-      name: "",
-      phone_number: "",
+      password: "",
+      confirmPassword: "",
     },
   });
 
@@ -48,22 +51,6 @@ export const BusinessSignUpForm = () => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="flex flex-col gap-4"
         >
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>{i18n.t("form.brand_name")}</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder={i18n.t("form.brand_name_placeholder")}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          ></FormField>
           <FormField
             control={form.control}
             name="email"
@@ -83,15 +70,12 @@ export const BusinessSignUpForm = () => {
           ></FormField>
           <FormField
             control={form.control}
-            name="phone_number"
+            name="password"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>{i18n.t("form.phone_number")}</FormLabel>
+                <FormLabel>{i18n.t("form.password")}</FormLabel>
                 <FormControl>
-                  <Input
-                    placeholder={i18n.t("form.phone_number_placeholder")}
-                    {...field}
-                  />
+                  <Input type="password" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -99,23 +83,21 @@ export const BusinessSignUpForm = () => {
           ></FormField>
           <FormField
             control={form.control}
-            name="description"
+            name="confirmPassword"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>
-                  {i18n.t("form.business_description.optional")}
-                </FormLabel>
+                <FormLabel>{i18n.t("form.confirm_password")}</FormLabel>
                 <FormControl>
-                  <Input
-                    placeholder={i18n.t("form.business_description")}
-                    {...field}
-                  />
+                  <Input type="password" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           ></FormField>
-          <a href={`/${i18n.language}/business-login`}>
+          <a
+            href={`/${i18n.language}/business-login`}
+            className="underline underline-offset-4"
+          >
             {i18n.t("form.business_signup_signin")}
           </a>
           <Button>{i18n.t("form.submit")}</Button>
