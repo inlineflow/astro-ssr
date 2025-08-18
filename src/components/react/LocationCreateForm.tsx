8f8c6a937768a63c0da0d55c3e0a4c36e2cccc1f
Danil Samsonov <thrashdev@protonmail.com>
2025-08-18 09:31:08 +0600

    WIP

---
+++ src/components/react/LocationCreateForm.tsx
@@ -0,0 +1,131 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/ui/form";
+import { z } from "zod";
+import i18n from "src/lib/i18n";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/ui/input";
+import { useForm } from "react-hook-form";
+import { Card } from "@/ui/card";
+import { Button } from "@/ui/button";
+import { locationTypes, locationTypeToServices } from "src/lib/schema";
+
+const FormSchema = z.object({
+  name: z.string().min(1, { message: i18n.t("form.location_name_empty") }),
+  type: z.enum(locationTypes, { message: i18n.t("form.location_type_empty") }),
+  services: z.enum(
+    Object.values(locationTypeToServices)
+      .map((lts) => lts.map((l) => l.serviceName))
+      .flat()
+  ),
+});
+
+type BusinessSignUpFormValues = z.infer<typeof FormSchema>;
+const onSubmit = async (data: BusinessSignUpFormValues) => {
+  console.log(data);
+};
+
+export const BusinessSignUpForm = () => {
+  const form = useForm<BusinessSignUpFormValues>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      description: "",
+      email: "",
+      name: "",
+      phone_number: "",
+    },
+  });
+
+  // return (<p>Hello from form</p>);
+
+  return (
+    <Card className=" w-full m-12 p-4">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-4"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{i18n.t("form.brand_name")}</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder={i18n.t("form.brand_name_placeholder")}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{i18n.t("form.brand_email")}</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder={i18n.t("form.brand_email_placeholder")}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            control={form.control}
+            name="phone_number"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{i18n.t("form.phone_number")}</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder={i18n.t("form.phone_number_placeholder")}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  {i18n.t("form.business_description.optional")}
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder={i18n.t("form.business_description")}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <a
+            href={`/${i18n.language}/business-login`}
+            className="underline underline-offset-4 text-sm"
+          >
+            {i18n.t("form.business_signup_signin")}
+          </a>
+          <Button>{i18n.t("form.submit")}</Button>
+        </form>
+      </Form>
+    </Card>
+  );
+};
